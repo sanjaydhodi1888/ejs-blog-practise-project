c0cc455579bd4b01a6f235b4001b9dc249936d34
sanjay dhodi <sdhodi157@gmail.com>
2023-03-16 17:41:32 +0530

    project first part complete without database

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 
+const _ = require("lodash");
+
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent =
@@ -52,17 +54,18 @@ app.post("/compose", (req, resp) => {
 });
 
 app.get("/post/:name", (req, resp) => {
-  let requestedData = req.params.name;
+  let requestedData = _.lowerCase(req.params.name);
+  let searchMatchingPost = [];
   postsArray.forEach((post) => {
-    if (requestedData === post.title) {
-      console.log("match found");
-    } else {
-      console.log("match not found");
+    if (requestedData === _.lowerCase(post.title)) {
+      let obj = {
+        title: post.title,
+        content: post.postbody,
+      };
+      searchMatchingPost.push(obj);
     }
   });
-
-  if (requestedData === "") {
-  }
+  resp.render("post", { searchMatchingPost: searchMatchingPost });
 });
 
 app.listen(1888, function () {
